0df32b916f21fe76672254be96f43a9301610c87
hyup98 <hyup98@naver.com>
2025-02-26 10:24:25 +0900

    ver 0.0.01

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appService = module.get<AppService>(AppService);
+    appController = module.get<AppController>(AppController);
+  });
+
+  it('should return "Hello, Nest.js!"', () => {
+    expect(appController.getHello()).toBe('Hello, Nest.js!');
+  });
+});
