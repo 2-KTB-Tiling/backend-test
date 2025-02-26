a58bf36e34b1f1b4e9811dc95e6352149a6326d3
hyup98 <hyup98@naver.com>
2025-02-26 21:33:51 +0900

    사용자 repoURL 저장기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,15 @@ import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 // 다른 모듈들은 추후 구현 후 추가
 // import { TilModule } from './til/til.module';
-// import { GithubModule } from './github/github.module';
+import { GithubModule } from './github/github.module';
 // import { NotionModule } from './notion/notion.module';
 
 @Module({
   imports: [
     ConfigModule,
     AuthModule,
+    GithubModule
     // TilModule,
-    // GithubModule,
     // NotionModule,
   ],
   controllers: [AppController], // 이 부분 추가
